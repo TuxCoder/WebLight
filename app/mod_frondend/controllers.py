cd5b9b676599079d65a3b682f2649a536d5b6ddc
Norbert Summer <git@o-g.at>
2016-06-13 01:15:25 +0200

    fix frontend controller

--- app/mod_frondend/controllers.py
+++ app/mod_frondend/controllers.py
@@ -10,9 +10,16 @@ mod_frontend = Blueprint('frontend', __name__, url_prefix='/')
 
 
 @mod_frontend.route('/', methods=['GET'])
-@login_required
 def index():
     return app.send_static_file('index.html')
+
+@mod_frontend.route('styles/<file>', methods=['GET'])
+def styles(file):
+    return app.send_static_file('styles/'+file)
+
+@mod_frontend.route('scripts/<file>', methods=['GET'])
+def scripts(file):
+    return app.send_static_file('scripts/'+file)
 
 @mod_frontend.route('csrf.js', methods=['GET'])
 def csrf():
