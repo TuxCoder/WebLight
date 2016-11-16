a80212c791d2c0365bd7d1de9972849ee36293d7
TuxCoder <git@o-g.at>
2016-11-16 11:50:33 +0100

    [!] fix csrf bug

--- frontend/src/app/index.run.js
+++ frontend/src/app/index.run.js
@@ -6,16 +6,16 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log, $http, $timeout) {
+  function runBlock($log, $http, $interval) {
     $log.debug('runBlock end');
 
     function UpdateCsrf() {
-      $http.get('/csrf.js', {cache: true}).then(function (resp) {
+      $http.get('/csrf.js', {cache: false}).then(function (resp) {
         $http.defaults.headers.post['X-CSRFToken'] = resp.data.csrftoken;
       });
     }
 
-    $timeout(UpdateCsrf, 60*5*1000); // each 5 min
+    $interval(UpdateCsrf, 60*5*1000); // each 5 min
     UpdateCsrf();
   }
 
